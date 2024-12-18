fa23dcf49a7945a57e9750273c73762acacdb95d
cabrera <edancabreraav@ittepic.edu.mx>
2024-12-17 18:22:29 -0700

    Iconos en el navbar

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -18,6 +18,28 @@ export function LoginIcon() {
   );
 }
 
+export function LogoutIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-user-x"
+    >
+      <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+      <circle cx="8.5" cy="7" r="4"></circle>
+      <line x1="18" y1="8" x2="23" y2="13"></line>
+      <line x1="23" y1="8" x2="18" y2="13"></line>
+    </svg>
+  );
+}
+
 export function HomeIcon() {
   return (
     <svg
@@ -74,6 +96,48 @@ export function DeleteIcon() {
     >
       <polyline points="3 6 5 6 21 6"></polyline>
       <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+    </svg>
+  );
+}
+
+export function ShoppingCartIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-shopping-cart"
+    >
+      <circle cx="9" cy="21" r="1"></circle>
+      <circle cx="20" cy="21" r="1"></circle>
+      <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+    </svg>
+  );
+}
+
+export function AddProductIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="feather feather-plus-circle"
+    >
+      <circle cx="12" cy="12" r="10"></circle>
+      <line x1="12" y1="8" x2="12" y2="16"></line>
+      <line x1="8" y1="12" x2="16" y2="12"></line>
     </svg>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { userContext } from "../context/userContext";
 import { productContext } from "../context/productContext";
 import { cartContext } from "../context/cartContext";
+import { LoginIcon, LogoutIcon, ShoppingCartIcon, HomeIcon, AddProductIcon } from "./Icons";
 
 const Navbar = () => {
   const { setShowLoginModal, user, setUser } = useContext(userContext);
@@ -15,37 +16,40 @@ const Navbar = () => {
       <Link to='/' className="logo"><h2>Hero-Shop</h2></Link>
       {user.username!==null && <p>Bienvenido <strong>{user.username}</strong></p>}
       <div className="navbar-buttons">
-      <Link to="/" className="navbar-button">Inicio</Link>
+      <Link to="/" className="navbar-button" title="Inicio"><HomeIcon/></Link>
       <Link to="/catalogo" className="navbar-button">Catálogo</Link>
       
       {/*Renderizado condicional del botón para iniciar/cerrar sesión*/}
       {user.username === null ? (
         <button
           className="navbar-button"
+          title="Iniciar Sesión"
           onClick={() => setShowLoginModal(true)}
         >
-          Iniciar Sesión
+          <LoginIcon/>
         </button>
       ) : (
         <div>
           <Link to='/'><button
             className="navbar-button"
-            onClick={() => {setUser({ email: null, username: null, role: null }); setIsOpen(false); setCart([]);} }
+            title="Cerrar Sesión"
+            onClick={() => {setUser({ email: null, username: null, role: null }); setIsOpen(false); setCart([]); alert(`¡Vuelve pronto ${user.username}!`);} }
           >
-            Cerrar Sesión
+            <LogoutIcon/>
           </button></Link>
           {user.role === "client" && (
-            <button className="navbar-button" onClick={toggleCart}>
-              Carrito
+            <button className="navbar-button" title="Ver carrito" onClick={toggleCart}>
+              <ShoppingCartIcon/>
             </button>
           )}
           {/*Renderizado condicional para mostrar el botón de Añadir producto sólo en caso de ser administrador*/}
           {user.role === "admin" && (
             <Link to='/catalogo'><button
               className="navbar-button"
+              title="Añadir producto"
               onClick={() => setShowAddProductModal(true)}
             >
-              Añadir producto
+              <AddProductIcon/>
             </button></Link>
           )}
         </div>
